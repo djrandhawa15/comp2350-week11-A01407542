c169aa14439b0eff1a45638b477e3616bd21ce3b
Dilraj <djrandhawa15@hotmail.com>
2025-03-26 11:07:19 -0700

    added code

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,31 +1,12 @@
-const mysql = require('mysql2/promise');
-
+const MongoClient = require("mongodb").MongoClient;
 const is_hosted = process.env.IS_HOSTED || false;
 
-const dbConfigHosted = {
-	host: "us-cdbr-east-03.cleardb.com",
-	user: "b1ab7fb2ee03bc",
-	password: "2a484a2d",
-	database: "heroku_3d208ad4bd6f421",
-	multipleStatements: false,
-	namedPlaceholders: true
-};
+const hostedURI = "mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.6brbu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const localURI = "mongodb://127.0.0.1/?authSource=admin&retryWrites=true&w=majority";
 
-const dbConfigLocal = {
-	host: "localhost",
-	user: "root",
-	password: "Password",
-	database: "lab_example",
-	multipleStatements: false,
-	namedPlaceholders: true
-};
-
-if (is_hosted) {
-	var database = mysql.createPool(dbConfigHosted);
-}
-else {
-	var database = mysql.createPool(dbConfigLocal);
-}
+const database = new MongoClient(is_hosted ? hostedURI : localURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 module.exports = database;
-		
